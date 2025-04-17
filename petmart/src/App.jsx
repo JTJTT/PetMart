e047abfdf47181e0995220d48d612d9060930a25
JTJTT <jt.tran911@gmail.com>
2025-04-17 00:47:18 -0400

    Small changes

--- petmart/src/App.jsx
+++ petmart/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from './components/header/header.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <body>
+      <div className="App">
+        <Header />
+      </div>
+    </body>
   );
 }
 
